295454d97cae41c49c2e89540ce6ce5455ed6c97
Oleksandr1301 <viss82@ukr.net>
2023-05-09 12:55:18 +0300

    add ModalClose

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -3,21 +3,20 @@ import PropTypes from 'prop-types';
 import { Backdrop, ImgBox } from './modal.styled';
 
 export const Modal = ({ onModalClose, largeImageURL }) => {
+  const handleKeyDown = e => {
+    if (e.key === 'Escape' || e.currentTarget === e.target) {
+      return onModalClose();
+    }
+  };
   useEffect(() => {
-    const handleKeyDown = e => {
-      if (e.key === 'Escape' || e.currentTarget === e.target) {
-        return onModalClose();
-      }
-    };
-
     window.addEventListener('keydown', handleKeyDown);
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [onModalClose]);
+  });
 
   return (
-    <Backdrop onClick={onModalClose}>
+    <Backdrop onClick={handleKeyDown}>
       <ImgBox>
         <img src={largeImageURL} alt="" />
       </ImgBox>
